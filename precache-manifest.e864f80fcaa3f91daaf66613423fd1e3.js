5abc49a5f9c302ab88e77295822f96df5e526a1c
Larysa <Larysa76@gmail.com>
2019-07-01 22:49:06 +0300

    Updates

---
+++ precache-manifest.e864f80fcaa3f91daaf66613423fd1e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6393c954de33a6994e980ee3a15caf7",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "76d966cb970ba7340841",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "5096245e5e5a430c80b6",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.bb032d54.chunk.js"
+  },
+  {
+    "revision": "76d966cb970ba7340841",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.880fa11a.chunk.js"
+  },
+  {
+    "revision": "549cb78f4abd9d607011",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime~main.0cda3cff.js"
+  }
+]);
